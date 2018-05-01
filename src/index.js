ff37daafdb864f6cece65fb245f5c716d79f3b1a
Audrius Siliunas <audriuss@wix.com>
2018-05-01 17:42:18 +0300

    added React and removed package-lock

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 import "./scss/main.scss";
 
-console.log('Hello world');
+const App = () => {
+    return <div>Hello World!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
